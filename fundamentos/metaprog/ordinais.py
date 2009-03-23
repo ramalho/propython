f668763d2f60bb8951443a87b5d8c4e7692297e3
Luciano Ramalho <luciano@ramalho.org>
2009-03-23 12:49:31 -0300

    reorg dos exemplos de metaprog

--- fundamentos/metaprog/ordinais.py
+++ fundamentos/metaprog/ordinais.py
@@ -1,42 +1,61 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 # coding: utf-8
 
-'''Exemplo de acesso e atribuição dinâmica de atributos de instância.
+'''
+Os itens contidos em objetos da classe Lista podem ser acessados pelos
+ordinais de 'primeiro' até 'decimo', ou por abreviaturas de três letras
+destes ordinais:
 
-    >>> n = Nova()
-    * self.__setattr__('b')
-    >>> print repr(n.a)
-    1
-    >>> print repr(n.b)
-    2
-    >>> print repr(n.c)
-    * self.__getattr__('c')
-    'c?'
-    >>> n.a = 10
-    * self.__setattr__('a')
-    >>> print repr(n.a)
-    10
-    >>> n.x = 99
-    * self.__setattr__('x')
-    >>> print repr(n.x)
-    99
+    >>> l = Lista([11, 22, 33, 44, 55])
+    >>> l.primeiro
+    11
+    >>> l.ter
+    33
+
+O último também pode ser acessado como um atributo, mas a implementação
+neste caso é bem diferente:
+
+    >>> l.ult
+    55
+
+Quando não sabemos o que fazer com o atributo acessado, é melhor levantar
+a tradicional exceção AttributeError:
+
+    >>> l.spam
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Lista' object has no attribute 'spam'
+    
 
 '''
 
-class Nova(object):
-    a = 1
+from itertools import count
 
-    def __init__(self):
-        self.b = 2
-
-    def __getattr__(self, nome):
-        print '* self.__getattr__(%r)' % nome
-        return nome + '?'
-
-    def __setattr__(self, nome, valor):
-        print '* self.__setattr__(%r)' % nome
-        self.__dict__[nome] = valor
+class Lista(list):
     
+    __ordinais = ('primeiro segundo terceiro quarto quinto sexto setimo'
+                  ' oitavo nono decimo').split()
+    __abrevs = [s[:3] for s in __ordinais]
+    
+    def __getattr__(self, atrib):
+        atr = atrib[:3]
+        for i, ordinal, abrev in zip(count(), self.__ordinais, self.__abrevs):
+            if atrib == ordinal or atr == abrev:
+                return self[i]
+        else:
+            msg = "'%s' object has no attribute '%s'"
+            raise AttributeError(msg % (self.__class__.__name__, atrib))
+    
+    @property        
+    def ultimo(self):
+        return self[-1]
+        
+    ult = ultimo    
+    
+        
 if __name__=='__main__':
     import doctest
-    doctest.testmod()
+    doctest.testmod()    
+    
+    
+
